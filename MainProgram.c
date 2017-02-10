66279e25da1f0191303322042deef8be2ac79fdf
pranavj1001 <pranavj1001@gmail.com>
2017-02-10 23:24:39 +0530

    add the ability to turn with the help of position control interrupts
    now the bot can make a precise turn and move to a particular distance

--- MainProgram.c
+++ MainProgram.c
@@ -13,6 +13,11 @@ unsigned char sharp, distance, adc_reading;
 unsigned int value;
 float BATT_Voltage, BATT_V;
 
+//necessary variables for the Positioning
+volatile unsigned long int ShaftCountLeft = 0; //to keep track of left position encoder
+volatile unsigned long int ShaftCountRight = 0; //to keep track of right position encoder
+volatile unsigned int Degrees; //to accept angle in degrees for turning
+
 //Function to configure the pins for motion
 void motion_pin_config (void){
 	DDRA = DDRA | 0x0F; //set direction of the PORTA 3 to PORTA 0 pins as output
@@ -41,12 +46,26 @@ void buzzer_pin_config (void){
 	PORTC = PORTC & 0xF7;		//Setting PORTC 3 logic low to turnoff buzzer
 }
 
+//Function to configure INT4 (PORTE 4) pin as input for the left position encoder
+void left_encoder_pin_config (void){
+	DDRE  = DDRE & 0xEF;  //Set the direction of the PORTE 4 pin as input
+	PORTE = PORTE | 0x10; //Enable internal pull-up for PORTE 4 pin
+}
+
+//Function to configure INT5 (PORTE 5) pin as input for the right position encoder
+void right_encoder_pin_config (void){
+	DDRE  = DDRE & 0xDF;  //Set the direction of the PORTE 4 pin as input
+	PORTE = PORTE | 0x20; //Enable internal pull-up for PORTE 4 pin
+}
+
 //Function to initialize ports
 void port_init(){
  	motion_pin_config();
 	lcd_port_config();
 	adc_pin_config();
 	buzzer_pin_config();
+	left_encoder_pin_config(); //left encoder pin config
+	right_encoder_pin_config(); //right encoder pin config
 }
 
 //Function to Initialize ADC
@@ -58,6 +77,32 @@ void adc_init(){
 	ADCSRA = 0x86;		//ADEN=1 --- ADIE=1 --- ADPS2:0 = 1 1 0
 }
 
+void left_position_encoder_interrupt_init (void){ //Interrupt 4 enable
+	cli(); //Clears the global interrupt
+	EICRB = EICRB | 0x02; // INT4 is set to trigger with falling edge
+	EIMSK = EIMSK | 0x10; // Enable Interrupt INT4 for left position encoder
+	sei();   // Enables the global interrupt
+}
+
+void right_position_encoder_interrupt_init (void){ //Interrupt 5 enable
+	cli(); //Clears the global interrupt
+	EICRB = EICRB | 0x08; // INT5 is set to trigger with falling edge
+	EIMSK = EIMSK | 0x20; // Enable Interrupt INT5 for right position encoder
+	sei();   // Enables the global interrupt
+}
+
+//ISR for right position encoder
+ISR(INT5_vect)
+{
+	ShaftCountRight++;  //increment right shaft position count
+}
+
+
+//ISR for left position encoder
+ISR(INT4_vect)
+{
+	ShaftCountLeft++;  //increment left shaft position count
+}
 
 //Function used for setting motor's direction
 void motion_set (unsigned char Direction){
@@ -105,6 +150,96 @@ void soft_right_2 (void){ //Left wheel stationary, Right wheel backward
 
 void stop (void){       //hard stop
 	motion_set(0x00);
+}
+
+//Function used for turning robot by specified degrees
+void angle_rotate(unsigned int Degrees){
+	float ReqdShaftCount = 0;
+	unsigned long int ReqdShaftCountInt = 0;
+
+	ReqdShaftCount = (float) Degrees/ 4.090; // division by resolution to get shaft count
+	ReqdShaftCountInt = (unsigned int) ReqdShaftCount;
+	ShaftCountRight = 0;
+	ShaftCountLeft = 0;
+
+	while (1)
+	{
+		if((ShaftCountRight >= ReqdShaftCountInt) | (ShaftCountLeft >= ReqdShaftCountInt))
+		break;
+	}
+	stop(); //Stop robot
+}
+
+//Function used for moving robot forward by specified distance
+void linear_distance_mm(unsigned int DistanceInMM){
+	float ReqdShaftCount = 0;
+	unsigned long int ReqdShaftCountInt = 0;
+
+	ReqdShaftCount = DistanceInMM / 5.338; // division by resolution to get shaft count
+	ReqdShaftCountInt = (unsigned long int) ReqdShaftCount;
+	
+	ShaftCountRight = 0;
+	while(1)
+	{
+		if(ShaftCountRight > ReqdShaftCountInt)
+		{
+			break;
+		}
+	}
+	stop(); //Stop robot
+}
+
+void forward_mm(unsigned int DistanceInMM){
+	forward();
+	linear_distance_mm(DistanceInMM);
+}
+
+void back_mm(unsigned int DistanceInMM){
+	back();
+	linear_distance_mm(DistanceInMM);
+}
+
+void left_degrees(unsigned int Degrees){
+	// 88 pulses for 360 degrees rotation 4.090 degrees per count
+	left(); //Turn left
+	angle_rotate(Degrees);
+}
+
+
+
+void right_degrees(unsigned int Degrees){
+	// 88 pulses for 360 degrees rotation 4.090 degrees per count
+	right(); //Turn right
+	angle_rotate(Degrees);
+}
+
+
+void soft_left_degrees(unsigned int Degrees){
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_left(); //Turn soft left
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_right_degrees(unsigned int Degrees){
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_right();  //Turn soft right
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_left_2_degrees(unsigned int Degrees){
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_left_2(); //Turn reverse soft left
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_right_2_degrees(unsigned int Degrees){
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_right_2();  //Turn reverse soft right
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
 }
 
 //This Function accepts the Channel Number and returns the corresponding Analog Value 
@@ -286,8 +421,7 @@ int main(){
 			}
 		}
 		
-		right();
-		_delay_ms(600);
+		right_degrees(90);
 		
 		delayTime = 4;
 		for(int i = 0; i < delayTime*4; i++){
@@ -322,8 +456,7 @@ int main(){
 			}
 		}
 		
-		right();
-		_delay_ms(600);
+		right_degrees(90);
 		
 	}
 }
